1f7ef167ed2a7af110d11bb344bc274a92381495
Christian <109503264+chrispy67@users.noreply.github.com>
2024-10-02 11:40:29 -0700

    moved clear_images() to plots.py

--- plots.py
+++ plots.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from dipep_potential import V_x
 import matplotlib.animation as animation
+import os
+
 
 # - All of these functions should take in arrays and spit out a graph.
 #   - but what if I want it to update EACH FRAME and plot LIVE data?
@@ -10,13 +12,28 @@ import matplotlib.animation as animation
 # Universal values | font size, colors, etc
 plt.rcParams["axes.grid"] = True  # Enables grid for all plots
 
+def clear_images(image_path, overwrite=True):
+    """
+    Because this application may be run many times, this is the best way to ensure that
+    the files produced by walker.py from previous runs to do not populate the image.
+    """
+    if os.path.exists(image_path):
+        if overwrite: #is True, keeping the files in place
+            os.remove(image_path)
+        
+        else: #write backup file as bck.fes_{n}.png where n is lowest common integer
+            while os.path.exists(os.path.join('static', f"bck.fes_{n}.png")):
+                n += 1
+            
+            backup_path = os.path.join('static', f"bck.fes_{n}.png")
+            os.rename(image_path, backup_path)
 
 
 # This should work for updating both scatter and line plots. For later!
 def update(frame, x_data, y_data, obj, plot_type='scatter'):
     # for each frame, update the data stored on each artist.
-    x = x_data[:frame]
-    y = y_data [:frame]
+    x = x_data[:frame:10]
+    y = y_data [:frame:10]
     
     if plot_type == 'scatter':
         offsets = np.column_stack((x, y))
@@ -56,16 +73,23 @@ def energy_time(energy, time):
         title='Potential Energy of Dihedral Angle')
     return (fig,scat)
 
-# this should be static and doesn't need updates
+#####-----Template for formatting images for the Flask app-----#####
+# - This is the simplest case, other functions will be different
 def fes(potential):
     fig, ax = plt.subplots()
     x = np.linspace(-np.pi, np.pi, 100)
-    V = V_x(x)
+    V = potential(x) # this is assuming np.poly1D function notation
     plot = ax.plot(x, V)
 
     ax.set(xlim=[-np.pi, np.pi], ylim=[0, 100], 
         xlabel = 'phi (radians)',
         ylabel='Change in Free Energy')
+
+    
+    
+    # MUST ADD TO EACH FUNCTION I USE IF I WANT to populate the Flask application
+    fig.savefig('static/fes.png')
+
     return (fig, plot)
 
 
@@ -97,7 +121,10 @@ def animate_md(V, hills, rads):
         ylabel='Change in Free Energy',
         title='Metadynamics Simulation of Alanine Dipeptide Dihedral Angle')
 
-    plt.show() #must be inside the function, NOT walker.py to pass animation
+    # This either takes a REALLY long time or isn't working 
+    # ani.save('MD_simulation.gif', writer='pillow', fps=30)
+    # plt.show() #must be inside the function, NOT walker.py to pass animation
+
     return ani  
 
 
