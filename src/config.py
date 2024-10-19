3c6bd01a320fd1a2b71ac2fa5c8a5f500773bd55
Christian <109503264+chrispy67@users.noreply.github.com>
2024-10-19 16:14:09 -0700

    REMOVED TIMESTEP SLIDER/CONFIG

--- src/config.py
+++ src/config.py
@@ -1,9 +1,8 @@
-steps = 2380286
+steps = 50000
 timestep = 0.02
-temp = 310
-x0 = 0.008407346410207
-mratio = None
+temp = 313
+x0 = 2.5
 metad = True
-w = 0.6
-delta = 0.7
-hfreq = 740
+w = 1.2
+delta = 0.2
+hfreq = 100
